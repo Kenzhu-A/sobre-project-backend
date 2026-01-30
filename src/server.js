866583d73f6230a1dfbd756d4184d1ca2f65a0e7
Kenzhu-A <kenzhu_aguilera@dlsl.edu.ph>
2026-01-30 15:20:43 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
